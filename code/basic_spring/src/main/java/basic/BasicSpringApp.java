99dfcc087ed8ebd972f362803bdcc12f27970eb7
Tim Goeke <tim.goeke@gmail.com>
2011-10-23 19:45:07 -0600

    Putting Spring config in place.

--- code/basic_spring/src/main/java/basic/BasicSpringApp.java
+++ code/basic_spring/src/main/java/basic/BasicSpringApp.java
@@ -1,9 +1,15 @@
 package basic;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class BasicSpringApp {
 
     public static void main( String[] args ) {
-        System.out.println( "Starting Up!");
+        System.out.println( "Loading Spring Config!");
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+
     }
 
 }
