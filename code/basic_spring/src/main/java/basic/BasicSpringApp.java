5a4fbbc4d4e7e75f52e690196aeb2fcdfff94e08
Tim Goeke <tim.goeke@gmail.com>
2011-10-23 20:25:50 -0600

    Added some comments.

--- code/basic_spring/src/main/java/basic/BasicSpringApp.java
+++ code/basic_spring/src/main/java/basic/BasicSpringApp.java
@@ -8,6 +8,12 @@ public class BasicSpringApp {
 
     public static void main( String[] args ) {
 
+        /*
+        *
+        *   Our little app builds a house and prints out the results.
+        *
+        */
+
         System.out.println( "Loading Spring Config!");
 
         ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
